96cf0728faec781344a9c11dd802561c065cef31
Jake Weinstein <jake@aospa.co>
2022-12-09 00:35:52 +0900

    sm8350-common: Do not balance msm_drm and kgsl_3d0 IRQs
    These are critical for display performance and should not be balanced
    in order to improve latency and responsiveness.
    
    And also affine them.
    
    Change-Id: If49ecb8757d133a7fad0d7946837b35403e57c2a

--- common.mk
+++ common.mk
@@ -347,6 +347,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.oplus-libperfmgr
 
+# Performance
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
