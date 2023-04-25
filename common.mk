PixelOS-Devices/device_xiaomi_lisa

sm8350-common: build custom sensors multihal

common.mk
@@ -385,7 +385,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.xiaomi_sm8350-multihal \
     vendor.qti.hardware.display.mapper@1.1.vendor
 
 PRODUCT_COPY_FILES += \

Xtended-Devices/device_oneplus_sm8350-common

Change-Id: Id2883c4b99e36cc6f3b05716f45cf8e5ba73ec5a

common.mk
@@ -369,7 +369,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.oplus-multihal \
     libsensorndkbridge \
     sensors.oplus
 